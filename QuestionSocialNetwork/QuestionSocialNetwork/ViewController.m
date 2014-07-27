e7163895db24968951346197981d2cc02d640ba0
SamKirkiles <samkirkiles@Sams-MacBook-Pro.local>
2014-07-27 13:16:05 -0400

    Initial Commit

---
+++ QuestionSocialNetwork/QuestionSocialNetwork/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QuestionSocialNetwork
+//
+//  Created by Sam Kirkiles on 7/27/14.
+//  Copyright (c) 2014 Sam Kirkiles. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ QuestionSocialNetwork/QuestionSocialNetwork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuestionSocialNetwork
+//
+//  Created by Sam Kirkiles on 7/27/14.
+//  Copyright (c) 2014 Sam Kirkiles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
