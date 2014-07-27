5cfb47c8c49725ee039927e6cbe05caca0438137
SamKirkiles <samkirkiles@Sams-MacBook-Pro.local>
2014-07-27 13:34:00 -0400

    Parse support added

--- QuestionSocialNetwork/QuestionSocialNetwork/AppDelegate.m
+++ QuestionSocialNetwork/QuestionSocialNetwork/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [Parse setApplicationId:@"enTC2Lo5cSQeV4il0pq5Vwe38d6Z8V2NpAo5aqLM"
+                  clientKey:@"tEcHepxpUZs4akfnWRxt4JhLSQPbbn1U4Hyubxh9"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     return YES;
 }
 
